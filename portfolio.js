18145b8956b5da7a9bf5f8cf01c01b4e7c4c0656
sleepercell38 <insanevishal07@gmail.com>
2024-08-18 07:32:09 -0700

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+downloadcv=document.querySelector(".events").addEventListener("click", ()=>{
+    alert("CV has been downloaded");
+})
+
+var typed = new Typed('#element', {
+    strings: [ 'SOFTWARE DEVELOPER', 'UX_UI DESIGNER','FRONTEND DEVELOPER'],
+    typeSpeed: 50,
+  });
+  
+
+  var typed = new Typed('#element1', {
+    strings: [ 'SOFTWARE DEVELOPER', 'UX_UI DESIGNER','FRONTEND DEVELOPER'],
+    typeSpeed: 50,
+  });
+  SUBMITBUTTON=document.querySelector("#formbtn").addEventListener("click", ()=>{
+    alert("YOUR RESPONSE HAS BEEN SUBMITTED!!");
+})
